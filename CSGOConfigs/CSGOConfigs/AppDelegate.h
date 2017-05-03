43d015bb6285a6c280a651e37a62b1d758873d3a
Oleg Petrychuk <olehpetruch@gmail.com>
2017-05-03 20:21:52 +0300

    Initial commit

---
+++ CSGOConfigs/CSGOConfigs/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  CSGOConfigs
+//
+//  Created by Петрічук Олег Аркадійовіч on 03.05.17.
+//  Copyright © 2017 Oleg Petruchyk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
