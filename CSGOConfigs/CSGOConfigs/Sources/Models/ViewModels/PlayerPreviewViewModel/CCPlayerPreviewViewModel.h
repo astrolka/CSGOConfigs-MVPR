aef87df42b4fd97545ec4c80072200dc37666e25
Oleg Petrychuk <olehpetruch@gmail.com>
2017-05-20 13:55:47 +0300

    PlayersService Added PlayersPreviewViewModelBuilder methods

--- CSGOConfigs/CSGOConfigs/Sources/Models/ViewModels/PlayerPreviewViewModel/CCPlayerPreviewViewModel.h
+++ CSGOConfigs/CSGOConfigs/Sources/Models/ViewModels/PlayerPreviewViewModel/CCPlayerPreviewViewModel.h
@@ -7,13 +7,33 @@
 //
 
 #import <Foundation/Foundation.h>
-
-@interface CCPlayerPreviewViewModelBuilder : NSObject
-
-
-@end
+#import <CoreGraphics/CGBase.h>
+@class CCPlayerPreviewServerModel;
+@class CCPlayerPreviewCoreDataModel;
 
 @interface CCPlayerPreviewViewModel : NSObject
 
+@property (nonatomic, strong) NSString *nickName;
+@property (nonatomic, strong) NSURL *imageURL;
+@property (nonatomic, assign) NSInteger playerID;
+@property (nonatomic, assign) CGFloat imageHeight;
+
 @end
+
+
+
+typedef void(^playersDataBlock)(NSArray <CCPlayerPreviewViewModel *> *players);
+
+@interface CCPlayerPreviewViewModelBuilder : NSObject
+
++ (void)buildViewModelsWithServerModels:(NSArray <CCPlayerPreviewServerModel *> *)serverModels
+                         containerWidth:(CGFloat)containerWidth
+                             viewModels:(playersDataBlock)viewModels;
+
++ (void)buildViewModelsWithCoreDataModels:(NSArray <CCPlayerPreviewCoreDataModel *> *)coreDataModels
+                           containerWidth:(CGFloat)containerWidth
+                               viewModels:(playersDataBlock)viewModels;
+
+@end
+
 
--- CSGOConfigs/CSGOConfigs/Sources/Models/ViewModels/PlayerPreviewViewModel/CCPlayerPreviewViewModel.m
+++ CSGOConfigs/CSGOConfigs/Sources/Models/ViewModels/PlayerPreviewViewModel/CCPlayerPreviewViewModel.m
@@ -7,13 +7,86 @@
 //
 
 #import "CCPlayerPreviewViewModel.h"
+#import "CCPlayerPreviewServerModel.h"
+#import "CCPlayerPreviewCoreDataModel+CoreDataProperties.h"
 
-@implementation CCPlayerPreviewViewModelBuilder
-
-
-@end
+NSString * const kPlayerPreviewImageSizeSeparateChar = @"x";  // responce format: "320x640"
 
 @implementation CCPlayerPreviewViewModel
 
 @end
 
+
+
+@implementation CCPlayerPreviewViewModelBuilder
+
+#pragma mark - Public
+
++ (void)buildViewModelsWithServerModels:(NSArray <CCPlayerPreviewServerModel *> *)serverModels
+                         containerWidth:(CGFloat)containerWidth
+                             viewModels:(playersDataBlock)viewModels {
+    
+    __block NSMutableArray <CCPlayerPreviewViewModel *> *viewModelsData = [NSMutableArray new];
+    
+    const char *classNameChar = [NSStringFromClass([CCPlayerPreviewViewModelBuilder class]) UTF8String];
+    dispatch_queue_t queue = dispatch_queue_create(classNameChar, DISPATCH_QUEUE_SERIAL);
+    
+    dispatch_async(queue, ^{
+        for (CCPlayerPreviewServerModel *serverModel in serverModels) {
+            CCPlayerPreviewViewModel *viewModel = [[CCPlayerPreviewViewModel alloc]init];
+            viewModel.nickName = serverModel.nickName;
+            viewModel.imageURL = serverModel.imageURL;
+            viewModel.playerID = serverModel.playerID;
+            viewModel.imageHeight = [self calculateImageHeightForImageSize:serverModel.imageSize containerWidth:containerWidth];
+            [viewModelsData addObject:viewModel];
+        }
+    });
+    
+    dispatch_async(queue, ^{
+        dispatch_async(dispatch_get_main_queue(), ^{
+            viewModels(viewModelsData);
+        });
+    });
+}
+
++ (void)buildViewModelsWithCoreDataModels:(NSArray <CCPlayerPreviewCoreDataModel *> *)coreDataModels
+                           containerWidth:(CGFloat)containerWidth
+                               viewModels:(playersDataBlock)viewModels {
+    
+    __block NSMutableArray <CCPlayerPreviewViewModel *> *viewModelsData = [NSMutableArray new];
+    
+    const char *classNameChar = [NSStringFromClass([CCPlayerPreviewViewModelBuilder class]) UTF8String];
+    dispatch_queue_t queue = dispatch_queue_create(classNameChar, DISPATCH_QUEUE_SERIAL);
+    
+    dispatch_async(queue, ^{
+        for (CCPlayerPreviewCoreDataModel *coreDataModel in coreDataModels) {
+            CCPlayerPreviewViewModel *viewModel = [[CCPlayerPreviewViewModel alloc]init];
+            viewModel.nickName = coreDataModel.nickName;
+            viewModel.imageURL = [NSURL URLWithString:coreDataModel.imageURL];
+            viewModel.playerID = coreDataModel.playerID.integerValue;
+            viewModel.imageHeight = [self calculateImageHeightForImageSize:coreDataModel.imageSize containerWidth:containerWidth];
+            [viewModelsData addObject:viewModel];
+        }
+    });
+    
+    dispatch_async(queue, ^{
+        dispatch_async(dispatch_get_main_queue(), ^{
+            viewModels(viewModelsData);
+        });
+    });
+}
+
+#pragma mark - Private
+
++ (CGFloat)calculateImageHeightForImageSize:(NSString *)imageSize containerWidth:(CGFloat)containerWidth {
+    NSArray *separateImageSizeArray = [imageSize componentsSeparatedByString:kPlayerPreviewImageSizeSeparateChar];
+    NSString *serverImageHeightStr = [separateImageSizeArray lastObject];
+    NSString *serverImageWidthStr = [separateImageSizeArray firstObject];
+    if (!serverImageHeightStr) {
+        return 0.f;
+    } else {
+        return (serverImageHeightStr.floatValue * containerWidth) / serverImageWidthStr.floatValue;
+    }
+}
+
+@end
